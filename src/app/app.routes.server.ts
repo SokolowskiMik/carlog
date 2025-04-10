566e235c24e7ae6282e75b79ac242779e5647401
Jakub Wojcik <wcuba@wp.pl>
2025-04-10 19:42:35 +0200

    render mode client

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,26 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: 'garage-form/:carId', renderMode: RenderMode.Client },
+  { path: 'current-car/:carId', renderMode: RenderMode.Client },
+  { path: 'insurance/:carId', renderMode: RenderMode.Client },
+  { path: 'insurance/:carId/:insuranceId', renderMode: RenderMode.Client },
+  { path: 'insurance-form/:carId', renderMode: RenderMode.Client },
+  { path: 'insurance-form/:carId/:insuranceId', renderMode: RenderMode.Client },
+  { path: 'fuel/:carId', renderMode: RenderMode.Client },
+  { path: 'fuel-details/:fuelId', renderMode: RenderMode.Client },
+  { path: 'fuel-form', renderMode: RenderMode.Client },
+  { path: 'fuel-form/:carId', renderMode: RenderMode.Client },
+  { path: 'fuel-form/:fuelId', renderMode: RenderMode.Client },
+  { path: 'car/:carId/fuel', renderMode: RenderMode.Client },
+  { path: 'car/:carId/fuel-details/:fuelId', renderMode: RenderMode.Client },
+  { path: 'car/:carId/fuel-form', renderMode: RenderMode.Client },
+  { path: 'car/:carId/fuel-form/:fuelId', renderMode: RenderMode.Client },
+  { path: 'notes', renderMode: RenderMode.Client },
+  { path: 'notes/:carId', renderMode: RenderMode.Client },
+  { path: 'notes-details/:carId/:noteId', renderMode: RenderMode.Client },
+  { path: 'notes-form', renderMode: RenderMode.Client },
+  { path: 'notes-form/:noteId', renderMode: RenderMode.Client },
   {
     path: '**',
     renderMode: RenderMode.Prerender
